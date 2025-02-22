4a936917ad2bcc4bd0a62770dbab5127d69b2ef1
cogdiver <valentinaarenas2@gmail.com>
2025-02-21 19:46:57 -0500

    add docker

--- yocelin/main.py
+++ yocelin/main.py
@@ -1,3 +1,27 @@
+import psycopg2
+
+# Configuración de conexión
+DB_CONFIG = {
+    "dbname": "mydatabase",
+    "user": "admin",
+    "password": "admin123",
+    "host": "localhost",  # O usa el nombre del contenedor si estás en otro servicio dentro de Docker
+    "port": "5432"
+}
+
+try:
+    conn = psycopg2.connect(**DB_CONFIG)
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+    print("Conectado a:", cursor.fetchone()[0])
+
+    # Cierra la conexión
+    cursor.close()
+    conn.close()
+except Exception as e:
+    print("Error al conectar:", e)
+
+
 
 def extract():
     pass
@@ -11,3 +35,4 @@ def load():
 
 def main():
     pass
+
